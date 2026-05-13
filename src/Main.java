36e2187a9bd94acb0a65046b90ed6a9a289ab7a0
Rubayet Sabbir Faruque <rubayetsabbir@gmail.com>
2026-05-14 02:14:29 +0800

    Finished Menu logic

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import csv.SensorData;
 import java.util.Scanner;
 import logger.Logger;
 
@@ -7,10 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean endProgram = false;
-
+        SensorData csvData = new SensorData("D:\\Curtin_university\\semester 1\\COMP1007\\Assignment\\data.csv");
 
         while (!endProgram) {
-            String menu = "Welcome to the Smart greenhouse Monitoring System."
+            String menu = "\nWelcome to the Smart greenhouse Monitoring System."
             + '\n' + "Please select an option:"
             + '\n' + "1. Statistics for the entire greenhouse"
             + '\n' + "2. Statistics by zone"
@@ -27,8 +28,8 @@ public class Main {
                 case 1 -> dataRange = "all";
                 case 2 -> dataRange = selectZone(scanner);
                 case 3 -> dataRange = selectSensorType(scanner);
-                case 4 -> addData();
-                case 5 -> deleteData();
+                case 4 -> addData(scanner);
+                case 5 -> deleteData(scanner);
                 case 6 -> endProgram = true;
             }
 
@@ -46,7 +47,13 @@ public class Main {
                 choice = getValidInputInRange(scanner, 1, 7);
 
                 switch(choice) {
-                    case 1 -> logger.log("Total data readings of " + dataRange + " is ");
+                    case 1 -> subOp1TotalCount(dataRange, csvData);
+                    case 2 -> subOp2MeanVal(dataRange, csvData);
+                    case 3 -> subOp3MinVal(dataRange, csvData);
+                    case 4 -> subOp4MaxVal(dataRange, csvData);
+                    case 5 -> subOp5UnsafeCount(dataRange, csvData);
+                    case 6 -> subOp6UnsafePercentage(dataRange, csvData);
+                    case 7 -> subOp7AllStat(dataRange, csvData);
                 }
             }
 
@@ -72,7 +79,7 @@ public class Main {
         }
     }
 
-
+    // ---------- MAIN MENU OPTIONS -------------------------
     public static String selectZone(Scanner scanner) {
         logger.log("\nSelect on of the following zones:"
             + '\n' + "1. ZoneA"
@@ -80,11 +87,69 @@ public class Main {
             + '\n' + "3. ZoneC"
             + '\n'
         );
-        
         return switch (getValidInputInRange(scanner, 1, 3)) {
             case 1 -> "ZoneA";
             case 2 -> "ZoneB";
             default -> "ZoneC";
         };
+    }
+
+    public static String selectSensorType(Scanner scanner) {
+        logger.log("\nSelect sensor type: "
+            + '\n' + "1. Temperature"
+            + '\n' + "2. Humidity"
+            + '\n' + "3. Soil Moiture"
+            + '\n' + "4. Light"
+        );
+        return switch (getValidInputInRange(scanner, 1, 4)) {
+            case 1 -> "temperature";
+            case 2 -> "humidity";
+            case 3 -> "soilMoisture";
+            default -> "light";
+        };
+    }
+
+    public static void addData(Scanner scanner) {
+
+    }
+
+    public static void deleteData(Scanner scanner) {
+
+    }
+
+
+    // ------------ SUB MENU ------------------------
+    public static void subOp1TotalCount(String dataRange, SensorData csvData) {
+        logger.log("Total number of " + dataRange + " sensor readings is " + csvData.getTotalCount(dataRange) + '\n');
+    }
+
+    public static void subOp2MeanVal(String dataRange, SensorData csvData) {
+        logger.log("The average reading value of " + dataRange + " sensors is " + csvData.getMeanValue(dataRange) + '\n');
+    }
+
+    public static void subOp3MinVal(String dataRange, SensorData csvData) {
+        logger.log("The lowest sensor reading of " + dataRange + " sensors is " + csvData.getMinValue(dataRange) + '\n');
+    }
+
+    public static void subOp4MaxVal(String dataRange, SensorData csvData) {
+        logger.log("The highest sensor reading value of " + dataRange + " sensors is " + csvData.getMaxValue(dataRange) + '\n');
+    }
+
+    public static void subOp5UnsafeCount(String dataRange, SensorData csvData) {
+        logger.log("Number of readings outside of safe range is " + csvData.getUnsafeReadingCount(dataRange) + '\n');
+    }
+
+    public static void subOp6UnsafePercentage(String dataRange, SensorData csvData) {
+        logger.log("Percentage of unsafe readings in " + dataRange + ": " + csvData.getUnsafeReadingPercent(dataRange) + '\n');
+        logger.log("Percentage of unsafe readings in " + dataRange + " compared to total: " + csvData.getUnsafeReadingPercent(dataRange) + '\n');
+    }
+
+    public static void  subOp7AllStat(String dataRange, SensorData csvData) {
+        subOp1TotalCount(dataRange, csvData);
+        subOp2MeanVal(dataRange, csvData);
+        subOp3MinVal(dataRange, csvData);
+        subOp4MaxVal(dataRange, csvData);
+        subOp5UnsafeCount(dataRange, csvData);
+        subOp6UnsafePercentage(dataRange, csvData);
     }
 }
